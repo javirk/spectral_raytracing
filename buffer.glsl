0a4117205c2f5a3ff7c4997126e0627a123f9ef1
Javier <jgamtejero@gmail.com>
2024-07-07 23:19:21 +0200

    Back to the original code

--- buffer.glsl
+++ buffer.glsl
@@ -6,7 +6,7 @@
 // I have combined different intersection routines in one shader (similar 
 // to "Raymarching - Primitives": https://www.shadertoy.com/view/Xds3zN) and
 // added a simple ray tracer to visualize a scene with all primitives.
-//
+// I should add the structs from here: https://www.shadertoy.com/view/tsKyWG
 
 #define PATH_LENGTH 12
 #iChannel0 "self"
@@ -14,49 +14,6 @@
 #define LOWER_BOUND 450
 #define UPPER_BOUND 750
 #define NUM_WAVELENGTHS 10
-
-#define ILLUMINANT_A 1.435e7 / (2848. * 560.) - 1.
-
-// Spectrum to xyz approx function from "Simple Analytic Approximations to the CIE XYZ Color Matching Functions"
-// http://jcgt.org/published/0002/02/01/paper.pdf and https://www.shadertoy.com/view/WlsXDj
-//Inputs:  Wavelength in nanometers
-float xFit_1931(float wave)
-{
-    float t1 = (wave - 442.0f)*((wave < 442.0f) ? 0.0624f : 0.0374f);
-    float t2 = (wave - 599.8f)*((wave < 599.8f) ? 0.0264f : 0.0323f);
-    float t3 = (wave - 501.1f)*((wave < 501.1f) ? 0.0490f : 0.0382f);
-    return 0.362f*exp(-0.5f*t1*t1) + 1.056f*exp(-0.5f*t2*t2) - 0.065f*exp(-0.5f*t3*t3);
-}
-float yFit_1931(float wave)
-{
-    float t1 = (wave - 568.8f)*((wave < 568.8f) ? 0.0213f : 0.0247f);
-    float t2 = (wave - 530.9f)*((wave < 530.9f) ? 0.0613f : 0.0322f);
-    return 0.821f*exp(-0.5f*t1*t1) + 0.286f*exp(-0.5f*t2*t2);
-}
-float zFit_1931(float wave)
-{
-    float t1 = (wave - 437.0f)*((wave < 437.0f) ? 0.0845f : 0.0278f);
-    float t2 = (wave - 459.0f)*((wave < 459.0f) ? 0.0385f : 0.0725f);
-
-    return 1.217f*exp(-0.5f*t1*t1) + 0.681f*exp(-0.5f*t2*t2);
-}
-
-vec3 XYZtosRGB(vec3 XYZ)
-{
-    vec3 rgb;
-    rgb.x = XYZ.x *  3.2409699f + XYZ.y * -1.5373832f + XYZ.z * -0.4986108f;
-    rgb.y = XYZ.x * -0.9692436f + XYZ.y *  1.8759675f + XYZ.z *  0.0415551f;
-    rgb.z = XYZ.x *  0.0556301f + XYZ.y * -0.2039770f + XYZ.z *  1.0569715f;
-    
-    return rgb;
-}
-
-float a_illuminant(float wave) {
-    float den = exp(1.435e7 / (2848. * wave)) - 1.;
-    float num = exp(ILLUMINANT_A);
-
-    return 100. * pow(560. / wave, 5.) * num / den;
-}
 
 //
 // Hash functions by Nimitz:
@@ -193,13 +150,6 @@ void getMaterialProperties(in vec3 pos, in float mat,
     }
 }
 
-float reflectivity_old(float n1, float n2, float cosTheta, float wavelenght) {
-    float r0 = (n1 - n2) / (n1 + n2);
-    r0 = r0*r0;
-    float exponent = 1. / (1. + 0.05 * (wavelenght - float(LOWER_BOUND)));
-    return r0 + (1. - r0) * (1. - pow(cosTheta, exponent));
-}
-
 float reflectivity(float n1_over_n2, float cosTheta, float wavelenght) {
     float r0 = (n1_over_n2 - 1.) / (n1_over_n2 + 1.);
     r0 = r0*r0;
@@ -214,117 +164,6 @@ float schlick(float cosine, float r0) {
     return r0 + (1. - r0) * pow(abs(1. - cosine), 5.);
 }
 
-float ray_trace(in vec3 ro, in vec3 rd, in float wavelenght, inout float seed) {
-    vec3 albedo, normal = vec3(1.);
-    float roughness, type;
-    float nt = 1.33;
-    float nt_nt = nt * nt;
-    vec3 col = vec3(1.);
-    float cosine = 1.;
-    int i_refracted = 0;
-    float ray_intensity = 1.;
-    for (int i=0; i < PATH_LENGTH; ++i) {
-    	vec3 res = worldhit(ro, rd, vec2(.0001, 100), normal);
-		if (res.z > 0.) {
-			ro += rd * res.y;
-       		
-            getMaterialProperties(ro, res.z, albedo, type, roughness);
-            
-            if (type < LAMBERTIAN+.5) { // Added/hacked a reflection term
-                float F = FresnelSchlickRoughness(max(0., -dot(normal, rd)), .04, roughness);
-                if (F > hash1(seed)) {
-                    rd = modifyDirectionWithRoughness(normal, reflect(rd,normal), roughness, seed);
-                } else {
-                    col *= albedo;
-			        rd = cosWeightedRandomHemisphereDirection(normal, seed);
-                }
-            } else if (type < METAL+.5) {
-                col *= albedo;
-                rd = modifyDirectionWithRoughness(normal, reflect(rd,normal), roughness, seed);
-                // ray_intensity *= reflectivity(1., dot(rd, normal), wavelenght);   
-            } else { // DIELECTRIC
-                vec3 normalOut, refracted;
-                float ni_over_nt, reflectProb = 1.;
-                
-                if (dot(rd, normal) > 0.) {
-                    // Ray is inside the object.
-                    normalOut = -normal;
-            		ni_over_nt = nt;
-                    cosine = dot(rd, normal);
-                    cosine = sqrt(1. - nt_nt - nt_nt * cosine * cosine);
-                } else {
-                    normalOut = normal;
-                    ni_over_nt = 1./nt;
-                    cosine = -dot(rd, normal);
-                }
-            
-	            // Refract the ray.
-	            refracted = refract(normalize(rd), normalOut, ni_over_nt);
-    	        
-        	    // Handle total internal reflection.
-                if(refracted != vec3(0)) {
-                	float r0 = (1. - ni_over_nt)/(1. + ni_over_nt);
-	        		reflectProb = FresnelSchlickRoughness(cosine, r0*r0, roughness);
-                }
-                
-                if (hash1(seed) <= reflectProb) {
-                    rd = reflect(rd, normal);
-                    ray_intensity *= reflectivity(ni_over_nt, cosine, wavelenght);
-                } else {
-                    rd = refracted;
-                    ray_intensity *= 1. - reflectivity(ni_over_nt, dot(rd, normalOut), wavelenght);
-                }
-                rd = modifyDirectionWithRoughness(-normalOut, rd, roughness, seed);
-            }
-        } else {
-            return ray_intensity;
-        }
-    }  
-    return ray_intensity;
-}
-
-float calculate_normalization_factor() {
-    float normalization_factor = 0.;
-    for (int i_wave=0; i_wave < NUM_WAVELENGTHS; i_wave++) {
-        float wave = float(LOWER_BOUND) + float((UPPER_BOUND-LOWER_BOUND) * i_wave) / float(NUM_WAVELENGTHS);
-        normalization_factor += yFit_1931(wave);
-    }
-    return normalization_factor;
-}
-
-vec3 render_new( in vec3 ro, in vec3 rd, inout float seed ) {
-    float color_x = 0.;
-    float color_y = 0.;
-    float color_z = 0.;
-    float k = 1. / calculate_normalization_factor();
-    
-    float wave;
-
-    // Loop over wavelengths
-    for (int i_wave=0; i_wave < NUM_WAVELENGTHS; i_wave++) {
-        wave = float(LOWER_BOUND) + float((UPPER_BOUND-LOWER_BOUND) * i_wave) / float(NUM_WAVELENGTHS);
-        //wave = 450.;
-        float intensity = ray_trace(ro, rd, wave, seed);
-        // intensity = 1.;
-        float illuminant = a_illuminant(wave);
-
-        color_x += intensity * xFit_1931(wave) * illuminant;
-        color_y += intensity * yFit_1931(wave) * illuminant;
-        color_z += intensity * zFit_1931(wave) * illuminant;
-    }
-    vec3 xyz = vec3(color_x, color_y, color_z);
-
-    xyz *= k;
-    float total = xyz.x + xyz.y + xyz.z;
-    xyz.x /= total;
-    xyz.y /= total;
-    xyz.z = 1. - xyz.x - xyz.y;
-
-    //xyz = vec3(xFit_1931(wave), yFit_1931(wave), zFit_1931(wave));
-    vec3 rgb = XYZtosRGB(xyz);
-    return rgb;
-    //return vec3(intensity,0,0);
-}
 
 vec3 render( in vec3 ro, in vec3 rd, inout float seed ) {
     vec3 albedo, normal, col = vec3(1.); 
@@ -418,7 +257,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         ro = ro + ca * vec3(randomInUnitDisk(seed), 0.)*.02;
         rd = normalize(fp - ro);
 
-        vec3 col = render_new(ro, rd, seed);
+        vec3 col = render(ro, rd, seed);
 
         if (reset) {
             fragColor = vec4(col, 1);
--- test.glsl
+++ test.glsl
@@ -31,8 +31,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     vec3 col = data.rgb / data.w;
     
     // gamma correction
-    // col = max( vec3(0), col - 0.004);
-    // col = (col*(6.2*col + .5)) / (col*(6.2*col+1.7) + 0.06);
+    col = max( vec3(0), col - 0.004);
+    col = (col*(6.2*col + .5)) / (col*(6.2*col+1.7) + 0.06);
     
     // Output to screen
     fragColor = vec4(col,1.0);
