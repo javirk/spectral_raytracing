c1341475168d43a8eae55a5251870501a94c2278
Javier <jgamtejero@gmail.com>
2024-07-10 19:43:23 +0200

    chore: Refactor code for better readability and maintainability

--- buffer.glsl
+++ buffer.glsl
@@ -15,23 +15,37 @@
 #define UPPER_BOUND 750
 #define NUM_WAVELENGTHS 10
 
+struct Ray
+{
+    vec3 origin;
+    vec3 direction;
+};
+
+struct Material
+{
+    int   materialType;
+    vec3  albedo;
+    float fuzz;
+    float refractionIndex;
+};
+
 //
 // Hash functions by Nimitz:
 // https://www.shadertoy.com/view/Xt3cDn
 //
 
-uint baseHash( uvec2 p ) {
+uint baseHash(uvec2 p) {
     p = 1103515245U*((p >> 1U)^(p.yx));
     uint h32 = 1103515245U*((p.x)^(p.y>>3U));
     return h32^(h32 >> 16);
 }
 
-float hash1( inout float seed ) {
+float hash1(inout float seed) {
     uint n = baseHash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
     return float(n)/float(0xffffffffU);
 }
 
-vec2 hash2( inout float seed ) {
+vec2 hash2(inout float seed) {
     uint n = baseHash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
     uvec2 rz = uvec2(n, n*48271U);
     return vec2(rz.xy & uvec2(0x7fffffffU))/float(0x7fffffff);
@@ -41,11 +55,11 @@ vec2 hash2( inout float seed ) {
 // Ray tracer helper functions
 //
 
-float FresnelSchlickRoughness( float cosTheta, float F0, float roughness ) {
+float FresnelSchlickRoughness(float cosTheta, float F0, float roughness) {
     return F0 + (max((1. - roughness), F0) - F0) * pow(abs(1. - cosTheta), 5.0);
 }
 
-vec3 cosWeightedRandomHemisphereDirection( const vec3 n, inout float seed ) {
+vec3 cosWeightedRandomHemisphereDirection(const vec3 n, inout float seed) {
   	vec2 r = hash2(seed);
 	vec3  uu = normalize(cross(n, abs(n.y) > .5 ? vec3(1.,0.,0.) : vec3(0.,1.,0.)));
 	vec3  vv = cross(uu, n);
@@ -57,7 +71,7 @@ vec3 cosWeightedRandomHemisphereDirection( const vec3 n, inout float seed ) {
     return normalize(rr);
 }
 
-vec3 modifyDirectionWithRoughness( const vec3 normal, const vec3 n, const float roughness, inout float seed ) {
+vec3 modifyDirectionWithRoughness(const vec3 normal, const vec3 n, const float roughness, inout float seed) {
     vec2 r = hash2(seed);
     
 	vec3  uu = normalize(cross(n, abs(n.y) > .5 ? vec3(1.,0.,0.) : vec3(0.,1.,0.)));
@@ -75,7 +89,7 @@ vec3 modifyDirectionWithRoughness( const vec3 normal, const vec3 n, const float 
     return dot(ret,normal) > 0. ? ret : n;
 }
 
-vec2 randomInUnitDisk( inout float seed ) {
+vec2 randomInUnitDisk(inout float seed) {
     vec2 h = hash2(seed) * vec2(1,6.28318530718);
     float phi = h.y;
     float r = sqrt(h.x);
@@ -86,23 +100,23 @@ vec2 randomInUnitDisk( inout float seed ) {
 // Scene description
 //
 
-vec3 rotateY( const in vec3 p, const in float t ) {
+vec3 rotateY(const in vec3 p, const in float t) {
     float co = cos(t);
     float si = sin(t);
     vec2 xz = mat2(co,si,-si,co)*p.xz;
     return vec3(xz.x, p.y, xz.y);
 }
 
-vec3 opU( vec3 d, float iResult, float mat ) {
+vec3 opU(vec3 d, float iResult, float mat) {
 	return (iResult < d.y) ? vec3(d.x, iResult, mat) : d;
 }
 
-vec3 worldhit( in vec3 ro, in vec3 rd, in vec2 dist, out vec3 normal ) {
+vec3 worldhit(in Ray ray, in vec2 dist, out vec3 normal) {
     vec3 tmp0, tmp1, d = vec3(dist, 0.);
     // Last argument in opU is material.
     
-    d = opU(d, iPlane(ro, rd, d.xy, normal, vec3(0,1,0), 0.), 1.);  // Metal
-    d = opU(d, iSphere(ro - vec3( 0,1, 0), rd, d.xy, normal, 1.), 10.);  // Dielectric
+    d = opU(d, iPlane(ray.origin, ray.direction, d.xy, normal, vec3(0,1,0), 0.), 1.);  // Metal
+    d = opU(d, iSphere(ray.origin - vec3( 0,1, 0), ray.direction, d.xy, normal, 1.), 10.);  // Dielectric
     return d;
 }
 
@@ -114,11 +128,11 @@ vec3 pal(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
     return a + b*cos(6.28318530718*(c*t+d));
 }
 
-float checkerBoard( vec2 p ) {
+float checkerBoard(vec2 p) {
    return mod(floor(p.x) + floor(p.y), 2.);
 }
 
-vec3 getSkyColor( vec3 rd ) {
+vec3 getSkyColor(vec3 rd) {
     vec3 col = mix(vec3(1), vec3(.5, .7, 1), .5 + .5 * rd.y);
     float sun = clamp(dot(normalize(vec3(-.4, .7, -.6)), rd), 0., 1.);
     col += vec3(1, .6, .1)*(pow(sun, 4.) + 10. * pow(sun, 32.));
@@ -136,8 +150,7 @@ float gpuIndepentHash(float p) {
     return fract(p);
 }
 
-void getMaterialProperties(in vec3 pos, in float mat, 
-                           out vec3 albedo, out float type, out float roughness) {
+void getMaterialProperties(in vec3 pos, in float mat, out vec3 albedo, out float type, out float roughness) {
     albedo = pal(mat*.59996323+.5, vec3(.5),vec3(.5),vec3(1),vec3(0,.1,.2));
 
     if( mat < 1.5 ) {            
@@ -165,56 +178,56 @@ float schlick(float cosine, float r0) {
 }
 
 
-vec3 render( in vec3 ro, in vec3 rd, inout float seed ) {
+vec3 render(in Ray ray, inout float seed) {
     vec3 albedo, normal, col = vec3(1.); 
     float roughness, type;
     
-    for (int i=0; i<PATH_LENGTH; ++i) {    
-    	vec3 res = worldhit( ro, rd, vec2(.0001, 100), normal );
+    for (int i = 0; i < PATH_LENGTH; ++i) {    
+    	vec3 res = worldhit(ray, vec2(.0001, 100), normal);
 		if (res.z > 0.) {
-			ro += rd * res.y;
+			ray.origin += ray.direction * res.y;
        		
-            getMaterialProperties(ro, res.z, albedo, type, roughness);
+            getMaterialProperties(ray.origin, res.z, albedo, type, roughness);
             
             if (type < LAMBERTIAN+.5) { // Added/hacked a reflection term
-                float F = FresnelSchlickRoughness(max(0.,-dot(normal, rd)), .04, roughness);
+                float F = FresnelSchlickRoughness(max(0.,-dot(normal, ray.direction)), .04, roughness);
                 if (F > hash1(seed)) {
-                    rd = modifyDirectionWithRoughness(normal, reflect(rd,normal), roughness, seed);
+                    ray.direction = modifyDirectionWithRoughness(normal, reflect(ray.direction,normal), roughness, seed);
                 } else {
                     col *= albedo;
-			        rd = cosWeightedRandomHemisphereDirection(normal, seed);
+			        ray.direction = cosWeightedRandomHemisphereDirection(normal, seed);
                 }
             } else if (type < METAL+.5) {
                 col *= albedo;
-                rd = modifyDirectionWithRoughness(normal, reflect(rd,normal), roughness, seed);            
+                ray.direction = modifyDirectionWithRoughness(normal, reflect(ray.direction, normal), roughness, seed);            
             } else { // DIELECTRIC
                 vec3 normalOut, refracted;
                 float ni_over_nt, cosine, reflectProb = 1.;
-                if (dot(rd, normal) > 0.) {
+                if (dot(ray.direction, normal) > 0.) {
                     normalOut = -normal;
             		ni_over_nt = 1.4;
-                    cosine = dot(rd, normal);
+                    cosine = dot(ray.direction, normal);
                     cosine = sqrt(1.-(1.4*1.4)-(1.4*1.4)*cosine*cosine);
                 } else {
                     normalOut = normal;
                     ni_over_nt = 1./1.4;
-                    cosine = -dot(rd, normal);
+                    cosine = -dot(ray.direction, normal);
                 }
             
 	            // Refract the ray.
-	            refracted = refract(normalize(rd), normalOut, ni_over_nt);
+	            refracted = refract(normalize(ray.direction), normalOut, ni_over_nt);
     	        
         	    // Handle total internal reflection.
                 if(refracted != vec3(0)) {
-                	float r0 = (1.-ni_over_nt)/(1.+ni_over_nt);
+                	float r0 = (1. - ni_over_nt)/(1. + ni_over_nt);
 	        		reflectProb = FresnelSchlickRoughness(cosine, r0*r0, roughness);
                 }
                 
-                rd = hash1(seed) <= reflectProb ? reflect(rd,normal) : refracted;
-                rd = modifyDirectionWithRoughness(-normalOut, rd, roughness, seed);            
+                ray.direction = hash1(seed) <= reflectProb ? reflect(ray.direction, normal) : refracted;
+                ray.direction = modifyDirectionWithRoughness(-normalOut, ray.direction, roughness, seed);            
             }
         } else {
-            col *= getSkyColor(rd);
+            col *= getSkyColor(ray.direction);
 			return col;
         }
     }  
@@ -242,7 +255,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     float fpd = data.x;
     if(all(equal(ivec2(fragCoord), ivec2(0)))) {
         // Calculate focus plane.
-        float nfpd = worldhit(ro, normalize(vec3(.5,0,-.5)-ro), vec2(0, 100), normal).y;
+        Ray focus_ray = Ray(ro, normalize(vec3(.5,0,-.5)-ro));
+        float nfpd = worldhit(focus_ray, vec2(0, 100), normal).y;
         fragColor = vec4(nfpd, iResolution.xy, iResolution.x);
     } else { 
         vec2 p = (-iResolution.xy + 2.*fragCoord - 1.)/iResolution.y;
@@ -257,7 +271,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         ro = ro + ca * vec3(randomInUnitDisk(seed), 0.)*.02;
         rd = normalize(fp - ro);
 
-        vec3 col = render(ro, rd, seed);
+        Ray ray = Ray(ro, rd);
+
+        vec3 col = render(ray, seed);
 
         if (reset) {
             fragColor = vec4(col, 1);
